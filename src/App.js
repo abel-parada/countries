35753d0e9c62f9c7a03b2c201880c6be864fe0ae
abel-parada <abel.parada.millan@gmail.com>
2021-11-23 14:52:23 +0200

    backup in branch

--- src/App.js
+++ src/App.js
@@ -36,38 +36,40 @@ class App extends Component {
 
     return (
       <div className="App">
-        <h1>THE WORLD IS YOUR OYSTER!</h1>
-        <input type="text" name="search" placeholder="Search for a country here" onChange={this.searchHandler}></input>
-        <div className="wrapper">
-          {this.state.data.filter((country)=>{
-            return country.name.toLowerCase().includes(this.state.searchInput.toLowerCase());
-          }).map((country, i) =>(
-            <div className="countries" key={i}>
-              <div className="cardhead">
-              <h2>{country.name}</h2>
-              <h3>{country.capital}</h3>
-              </div>
-              <img src={country.flags.png} alt={country.name}/>
+        <div className="bg">
+          <h1>THE WORLD IS YOUR OYSTER!</h1>
+          <input type="text" name="search" placeholder="Search for a country here" onChange={this.searchHandler}></input>
+          <div className="wrapper">
+            {this.state.data.filter((country)=>{
+              return country.name.toLowerCase().includes(this.state.searchInput.toLowerCase());
+            }).map((country, i) =>(
+              <div className="countries" key={i}>
+                <div className="cardhead">
+                <h2>{country.name}</h2>
+                <h3>{country.capital}</h3>
+                </div>
+                <img src={country.flags.png} alt={country.name}/>
 
-              <p>
-                <strong>Languages</strong>: {country.languages.map((languages,j) => (
-                <span key={j}>{languages.name} </span>
-                ))}
-              </p>
-
-              <p>
-                <strong>Currencies</strong>: {country.currencies.map((currencies, k) => (
-                <span key={k}>{currencies.name} - {currencies.symbol}</span>
-                ))}
+                <p>
+                  <strong>Languages</strong>: {country.languages.map((languages,j) => (
+                  <span key={j}>{languages.name} </span>
+                  ))}
                 </p>
 
-              <p>
-                <strong>Population</strong>: {number.formatNumber(country.population)}</p>
-              
-              </div>
-          ))}
+                <p>
+                  <strong>Currencies</strong>: {country.currencies.map((currencies, k) => (
+                  <span key={k}>{currencies.name} - {currencies.symbol}</span>
+                  ))}
+                  </p>
+
+                <p>
+                  <strong>Population</strong>: {number.formatNumber(country.population)}</p>
+                
+                </div>
+            ))}
+          </div>
         </div>
-      </div>
+    </div>
     );
   }
 }
